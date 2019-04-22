6f8627e06bad7c6630c37f796e07dc8235df9ce8
Jamal Shareef <jamalkshareef@gmail.com>
2019-04-22 22:32:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
           if flavor == "strawberry"
-            [:favorite_icecream_flavors].delete("strawberry")
+            value.delete("strawberry")
           end
         end
       end
